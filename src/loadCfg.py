3d24db02d155d2a0c7460e5093756278e375f228
zombie <zombie@siisvae.com>
2016-12-22 14:33:42 -0500

    [DEV] Add a recursive function to read yml file with [key]->[key]->....[]->value structure

--- src/loadCfg.py
+++ src/loadCfg.py
@@ -6,6 +6,7 @@
 
 import constants as cts
 import ruamel.yaml as yaml
+import ruamel.yaml.comments as ymlClass
 
 class loadConfig():
 
@@ -22,3 +23,26 @@ class loadConfig():
             self.__cfg = yaml.load(open(cts.configFileName,'r').read(), yaml.RoundTripLoader)
         except yaml.YAMLError as exc:
             print('Error during load yml config file' + exc)
+
+
+    def get(self, key, inside = None):
+        if isinstance(key, ymlClass.CommentedMap):
+            if inside != None:
+                if len(inside.split('->')) > 1:
+                    return self.get(key[inside.split('->')[0]], inside[inside.find(inside.split('->')[1]):len(inside)])
+                else:
+                    return key[inside]
+        else:
+            if len(key.split('->')) > 1:
+                return self.get(self.__cfg[key.split('->')[0]], key[key.find(key.split('->')[1]):len(key)])
+            else:
+                if isinstance(self.__cfg[key], ymlClass.CommentedMap):
+                    return iter(self.__cfg[key])
+                else:
+                    return self.__cfg[key]
+
+
+
+test = loadConfig()
+# print(test.get('remote->host->sshKeyFile'))
+print(test.get('repos->folder'))
